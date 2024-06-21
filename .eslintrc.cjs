f1a599464a253a972ce0096adcb725c91e0e8a3f
iamidlek <iamidlek@naver.com>
2024-06-21 16:44:42 +0900

    init setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "babel-parser",
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Intro } from "./pages/Intro";
+import { Invitation } from "./pages/Invitation";
+import { Interview } from "./pages/Interview";
+
+function App() {
+  return (
+    <>
+      <Intro />
+      <Invitation />
+      <Interview />
+    </>
+  );
+}
+
+export default App;
